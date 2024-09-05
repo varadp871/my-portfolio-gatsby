7ba6cb17cfa1c68a9fd5556bd31c1983396fc5d6
Varad <panke.varad@gmail.com>
2024-09-05 23:50:26 +0530

    Added images to about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar";
 import '../styles/About.css';
+import team from '../images/undraw_engineering_team_a7n2.svg'
 
 function About() {
   return (
@@ -8,6 +9,7 @@ function About() {
       <Navbar />
       <div className="about-section">
       <p><h1>About me</h1></p>
+      <img src={team}></img>
       </div>
     </>
   );
